5d6c1e496d26b7647347f8b6c2b49c511cdebd64
TAlkingJosh <139813591+TAlkingJosh@users.noreply.github.com>
2025-06-17 11:30:22 -0500

    Update __init__.py

--- widgets/__init__.py
+++ widgets/__init__.py
@@ -4,3 +4,5 @@ from .gradient_editor_widget import GradientEditorWidget
 from .layout_overview_widget import LayoutOverviewWidget
 from .embedded_stage_view_widget import EmbeddedStageViewWidget
 from .embedded_timeline_widget import EmbeddedTimelineWidget
+from .programmer_view_widget import ProgrammerViewWidget
+from .cue_list_widget import CueListWidget
